eda526ec893d5488f1d4877de4cc29c186af429e
kateLee <xoolwater@gmail.com>
2021-07-10 17:17:32 +0800

    Add menu icon: favorite, search

--- app/src/main/java/kate/tutorial/turtlesoup/MainActivity.kt
+++ app/src/main/java/kate/tutorial/turtlesoup/MainActivity.kt
@@ -45,6 +45,8 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
+            R.id.action_favorite -> true
+            R.id.action_search -> true
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
